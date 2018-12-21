8f719b098169de2e110e717b2fffb4000a1b24a3
blackhonda2015 <blackhonda2015@gmail.com>
2018-12-21 18:29:02 -0500

    This is my ini commit

--- src/test/java/com/syntax/runners/TestRunner.java
+++ src/test/java/com/syntax/runners/TestRunner.java
@@ -1,15 +1,13 @@
 package com.syntax.runners;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features= {"src/test/resources/features"}
-				, glue= {"com/syntax/stepDefinitions"}
+				, glue= {""}
 				, dryRun=false)
 
 public class TestRunner {
 
 }
+
