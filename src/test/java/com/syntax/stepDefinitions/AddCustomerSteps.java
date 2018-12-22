552e37ff67add37ac8bd579ce5e9c0a90378dcfa
blackhonda2015 <blackhonda2015@gmail.com>
2018-12-22 14:14:30 -0500

    class project

--- src/test/java/com/syntax/stepDefinitions/AddCustomerSteps.java
+++ src/test/java/com/syntax/stepDefinitions/AddCustomerSteps.java
@@ -5,15 +5,12 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 
 import com.syntax.pages.AddCustomer;
 import com.syntax.pages.LoginPage;
 import com.syntax.utils.BaseClass;
 import com.syntax.utils.CommonMethods;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -22,7 +19,7 @@ public class AddCustomerSteps {
 
 	AddCustomer newCust;
 	WebDriver driver;
-	public static String name;
+	public String name;
 
 	@Given("^I logged into OrangeHRM$")
 	public void i_logged_into_OrangeHRM() throws Throwable {
@@ -55,30 +52,34 @@ public class AddCustomerSteps {
 
 	@When("^I add \"([^\"]*)\" and \"([^\"]*)\"$")
 	public void i_enter_Customer_Name_and_Description(String name, String description) {
-
+//		CommonMethods.enterValue(newCust.customerName, BaseClass.prop.getProperty("name"));
+//		CommonMethods.enterValue(newCust.description, BaseClass.prop.getProperty("description"));
 		CommonMethods.enterValue(newCust.customerName, name);
 		CommonMethods.enterValue(newCust.description, description);
 
 	}
 
 	@When("^I click Save$")
-	public void i_click_Save() {
+	public void i_click_Save() throws InterruptedException {
 		CommonMethods.click(newCust.saveButton);
+		Thread.sleep(5000);
 	}
 
-	@Then("^I see that the customer is displayed in the Customers table$")
-	public void i_see_that_the_customer_is_displayed_in_the_Customers_table() {
-	
-		List<WebElement> verifyAdd = driver.findElements(By.xpath("//table[@id=‘resultTable’]/tbody/tr/td/a"));
+	@Then("^I see that the \"([^\"]*)\" is displayed in the Customers table$")
+	public void i_see_that_the_is_displayed_in_the_Customers_table(String name) {
+
+		System.out.println(name);
+		List<WebElement> verifyAdd = driver.findElements(By.xpath("//table[@id='resultTable']/tbody/tr/td/a"));
 		for (int i = 0; i < verifyAdd.size(); i++) {
 			String text = verifyAdd.get(i).getText();
-			if (!text.equals("toyota")) {
+			if (!text.equals(name)) {
 				System.out.println("customer not added");
-			}else {
+			} else {
 				System.out.println("Customer added");
 
 			}
 
 		}
+
 	}
 }
--- src/test/resources/features/AddCustomer.feature
+++ src/test/resources/features/AddCustomer.feature
@@ -1,18 +1,17 @@
 #Author: SyntaxTeam
-
 Feature: Add customer
 
-Background: 
-Given I logged into OrangeHRM
+  Background: 
+    Given I logged into OrangeHRM
 
-Scenario:
+  Scenario Outline: 
+    And I am on Time Module
+    And I click Project info and select Customers
+    And I click on Add button
+    And I add "<name>" and "<description>"
+    And I click Save
+    Then I see that the "<name>" is displayed in the Customers table
 
-And I am on Time Module
-And I click Project info and select Customers
-And I click on Add button
-And I add "toyota" and "test"
-And I click Save
-Then I see that the customer is displayed in the Customers table
-
-
-
+    Examples: 
+      | name      | description |
+      | helllllo | world       |
